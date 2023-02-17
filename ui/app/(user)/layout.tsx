eac21fe45cba880734a4fd1526227bb7dfe80255
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-02-17 13:49:05 +0100

    fix/restructure

--- ui/app/(user)/layout.tsx
+++ ui/app/(user)/layout.tsx
@@ -1,3 +1,6 @@
+import Header from "components/shared/Header";
+import "../../styles/globals.css";
+
 export default function RootLayout({
   children,
 }: {
@@ -10,7 +13,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
