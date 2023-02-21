adfddfe070306a63c73be101228c8929a21c250b
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-02-21 17:24:29 +0100

    refactor to studio subfolder

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,15 +2,23 @@ import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
+import { media } from 'sanity-plugin-media'
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+
+console.log('PROJECT ID: ', projectId)
+console.log('DATASET: ', dataset)
 
 export default defineConfig({
-  name: 'default',
-  title: 'studio',
+  basePath: '/studio',
+  name: 'hoyre_dev_admin_panel',
+  title: 'Høyre dev admin panel',
 
-  projectId: '1hwvyivq',
-  dataset: 'production',
+  projectId: projectId ? projectId : '',
+  dataset: dataset ? dataset : '',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(), media()],
 
   schema: {
     types: schemaTypes,
