ac9bdbb2379df948a8ffa3cc2b7a9beb8288cbc8
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-10 14:22:27 +0200

    cahnge hover effect summerWSissel cards

--- components/sommerMedSissel/SummerPostCard.tsx
+++ components/sommerMedSissel/SummerPostCard.tsx
@@ -16,7 +16,7 @@ const builder = imageUrlBuilder(client)
 
 export default function SummerPostCard(post: summerPostCardProps) {
   return (
-    <>
+    <div className="group">
       <hr className="sm:block hidden mb-16" />
       <Link href={`sommer-med-sissel/${post.slug.current}`} className="flex items-center">
         <article className="relative isolate flex flex-col gap-6 lg:gap-8 lg:flex-row">
@@ -24,7 +24,7 @@ export default function SummerPostCard(post: summerPostCardProps) {
             <img
               src={builder.image(post.image).url()}
               alt=""
-              className="absolute inset-0 h-full w-full rounded-t-2xl lg:rounded-2xl bg-gray-50 object-cover"
+              className="absolute inset-0 h-full w-full rounded-t-2xl lg:rounded-2xl bg-gray-50 object-cover sm:opacity-90 group-hover:opacity-100"
             />
             <div className="absolute inset-0 rounded-t-2xl lg:rounded-2xl ring-1 ring-inset ring-gray-900/10" />
           </div>
@@ -40,16 +40,23 @@ export default function SummerPostCard(post: summerPostCardProps) {
               {post.category.title}
             </a> */}
             </div>
-            <div className="group relative max-w-xl">
+            <div className="relative max-w-xl ">
               <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                 <span className="absolute inset-0" />
                 {post.title}
               </h3>
-              <p className="mt-5 text-sm leading-6 text-gray-600">{post.description}</p>
+              {/* <p className="line-clamp-2 text-gray-500 text-lg 2xl:text-xl">{stage.shortDesc}</p> */}
+
+              <p className="mt-5 line-clamp-4 text-sm leading-6 text-gray-600">
+                {post.description}
+              </p>
+              <p className="flex items-center mt-5 font-bold group-hover:underline text-gray-900 group-hover:text-gray-600">
+                Les mer
+              </p>
             </div>
           </div>
         </article>
       </Link>
-    </>
+    </div>
   )
 }
