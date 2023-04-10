42c958931d3cd9059842bbd74bcfe557ded7a72c
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-10 22:32:11 +0200

    fix sortMenu

--- components/externalArticlesPage/ExternalArticlesList.tsx
+++ components/externalArticlesPage/ExternalArticlesList.tsx
@@ -7,15 +7,11 @@ import { groq } from 'next-sanity'
 import { useEffect, useState } from 'react'
 import { client } from 'sanity-conf/sanity.client'
 
-var initialPost = [
+const initialPost = [
   {
     _id: '1',
     title: 'Boost your conversion rate',
-    categories: [
-      { title: 'Category 1', description: 'Description 1' },
-      { title: 'Category 2', description: 'Description 2' },
-      { title: 'Category 4', description: 'Description 4' },
-    ],
+    categories: [{ _id: 'subject-1' }, { _id: 'subject-2' }],
     publisher: 'Publisher 1',
     description: 'Description 1',
     date: '2020-11-30',
@@ -23,49 +19,60 @@ var initialPost = [
   },
 ]
 
+interface Subject {
+  title: string
+  _id: string
+}
+
 export default function ExternalArticlesList() {
-  const [subjects, setSubjects] = useState([])
+  const [subjects, setSubjects] = useState<Subject[]>([{ title: '', _id: '' }])
   const [articles, setArticles] = useState(initialPost)
   const [sort, setSort] = useState<string>('desc')
   const [selectedSubjects, setSelectedSubjects] = useState<string[]>([])
 
-  const subjectsQuery = groq`
-    *[_type == "subject"] {
-    title
-    }`
+  // fetch subjects
   useEffect(() => {
+    const subjectsQuery = groq`
+    *[_type == "subject"] {
+    title, _id
+    }`
+
     const fetchSubjects = async () => {
       const result = await client.fetch(subjectsQuery)
       setSubjects(result)
     }
-
     fetchSubjects()
   }, [])
 
+  // fetch articles, refetch when sort order or selected subjects changes
   useEffect(() => {
     const articlesQuery = groq`
 *[_type == "externalArticle"] {
     _id,
     title,
-    categories,
+    categories[] -> {
+        _id,
+    },
     publisher,
     description,
     date,
     externalLink
     } | order(date ${sort})
     `
+
     const fetchArticles = async () => {
       const result = await client.fetch(articlesQuery)
       setArticles(result)
     }
-
     fetchArticles()
   }, [sort, selectedSubjects])
 
-  if (articles.length === 1)
+  // only show if articles are loaded
+  if (articles.length === 1) {
     return (
       <div className="max-w-3xl mx-auto mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16"></div>
     )
+  }
 
   const onRemove = (subject: string) => {
     setSelectedSubjects(selectedSubjects.filter((sub) => sub !== subject))
@@ -81,15 +88,20 @@ export default function ExternalArticlesList() {
     setSelectedSubjects([])
   }
 
+  const findSubjectById = (ref: string) => {
+    return subjects.find((subject) => subject._id === ref)?.title
+  }
+
   return (
     <div className="max-w-3xl mx-auto mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
       <div className="flex flex-row gap-x-4 items-center flex-wrap">
+        {/* <SortMenu sort={sort} setSort={setSort} /> */}
         <SortMenu sort={sort} setSort={setSort} />
-        <SubjectsMenu
+        {/* <SubjectsMenu
           subjects={subjects}
           selectedSubjects={selectedSubjects}
           onAddSubject={onAddSubject}
-        />
+        /> */}
         <div className="w-fit h-16 items-center">
           <button
             onClick={() => onRemoveAll()}
@@ -99,7 +111,6 @@ export default function ExternalArticlesList() {
             Nullstill filtre
           </button>
         </div>
-
         {selectedSubjects.length > 0 &&
           selectedSubjects.map((sub) => (
             <div key={sub} className="w-fit max-w-lg h-16">
@@ -109,23 +120,24 @@ export default function ExternalArticlesList() {
       </div>
       {articles.map((post) => (
         <article key={post._id} className="z-1 flex max-w-xl flex-col items-start justify-between">
-          <div className="flex items-center gap-x-4 text-xs">
+          <div className="flex items-center gap-x-4 text-xs flex-wrap">
             <time dateTime={post.date} className="text-gray-500">
               {post.date}
             </time>
-            {/* <a
-                    href={post.categories.href}
-                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                  >
-                    {post.categories.title}
-                  </a> */}
+            <div className="h-4 w-0.5 bg-gray-300" />
+            <p className=" text-gray-500">{post.publisher}</p>
+            {post.categories.map((category) => (
+              <div
+                key={category._id}
+                className="relative z-10 rounded-full bg-blue-50 px-3 py-1.5 font-medium text-blue-600 "
+              >
+                {findSubjectById(category._id)}
+              </div>
+            ))}
           </div>
           <div className="group relative">
             <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-              {/* <a href={post.externalLink}>
-                <span className="absolute inset-0" />
-                {post.title}
-              </a> */}
+              {post.title}
             </h3>
             <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
           </div>
