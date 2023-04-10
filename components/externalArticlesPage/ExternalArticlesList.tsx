e712503d3eaeb3b06ff1dec1f1b4def879bf0841
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-10 23:35:58 +0200

    style fixes

--- components/externalArticlesPage/ExternalArticlesList.tsx
+++ components/externalArticlesPage/ExternalArticlesList.tsx
@@ -4,6 +4,7 @@ import SelectedFilter from 'components/shared/SelectedFilter'
 import SortMenu from 'components/shared/SortMenu'
 import SubjectsMenu from 'components/shared/SubjectsMenu'
 import { groq } from 'next-sanity'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { client } from 'sanity-conf/sanity.client'
 
@@ -70,7 +71,7 @@ export default function ExternalArticlesList() {
   // only show if articles are loaded
   if (articles.length === 1) {
     return (
-      <div className="max-w-3xl mx-auto mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16"></div>
+      <div className="max-w-3xl mx-auto mt-10 space-y-16 border-gray-200 pt-10 sm:mt-16 sm:pt-16"></div>
     )
   }
 
@@ -98,7 +99,7 @@ export default function ExternalArticlesList() {
   })
 
   return (
-    <div className="max-w-3xl mx-auto mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
+    <div className="max-w-3xl mx-auto space-y-10 border-gray-200 pt-10 sm:pt-16">
       <div className="flex flex-row gap-x-4 items-center flex-wrap">
         <SortMenu sort={sort} setSort={setSort} />
         <SubjectsMenu
@@ -107,11 +108,9 @@ export default function ExternalArticlesList() {
           onAddSubject={onAddSubject}
         />
         <div className="w-fit h-16 items-center">
-          {/* TODO: Finn ut hvorfor denne får oransje farge on focus */}
           <button
             onClick={() => onRemoveAll()}
-            type="button"
-            className="px-3 py-2 block w-fulltext-base border border-gray-300 hover:outline-none hover:text-red-500 hover:ring-red-500 hover:border-red-500 sm:text-sm rounded-md focus-visible:border-primary focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-primary"
+            className="px-3 py-2 block w-fulltext-base border border-gray-300 hover:outline-none hover:text-red-500 hover:ring-red-500 hover:border-red-500 sm:text-sm rounded-md focus-visible:border-primary "
           >
             Nullstill filtre
           </button>
@@ -124,45 +123,51 @@ export default function ExternalArticlesList() {
           ))}
       </div>
       {articles.map((post) => (
-        <article key={post._id} className="z-1 flex max-w-xl flex-col items-start justify-between">
-          <div className="flex items-center gap-x-4 text-xs flex-wrap">
-            <time dateTime={post.date} className="text-gray-500">
-              {post.date}
-            </time>
-            <div className="h-4 w-0.5 bg-gray-300" />
-            <p className=" text-gray-500">{post.publisher}</p>
-            {post.categories.map((category) => (
-              <div
-                key={category._id}
-                className="relative z-10 rounded-full bg-blue-50 px-3 py-1.5 font-medium text-blue-600 "
-              >
-                {findSubjectById(category._id)}
-              </div>
-            ))}
-          </div>
-          <div className="group relative">
-            <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-              {post.title}
-            </h3>
-            <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
-          </div>
-          <div className="relative mt-8 flex items-center gap-x-4">
-            {/* <img
-                    src={post.author.imageUrl}
-                    alt=""
-                    className="h-10 w-10 rounded-full bg-gray-50"
-                  /> */}
-            <div className="text-sm leading-6">
-              {/* <p className="font-semibold text-gray-900">
+        <>
+          <hr />
+          <article
+            key={post._id}
+            className="z-1 flex max-w-xl flex-col items-start justify-between mb-16"
+          >
+            <div className="flex items-center gap-x-4 text-xs flex-wrap">
+              <time dateTime={post.date} className="text-gray-500">
+                {post.date}
+              </time>
+              <div className="h-4 w-0.5 bg-gray-300" />
+              <p className=" text-gray-500">{post.publisher}</p>
+              {post.categories.map((category) => (
+                <div
+                  key={category._id}
+                  className="relative z-10 rounded-full bg-blue-50 px-3 py-1.5 font-medium text-blue-600 "
+                >
+                  {findSubjectById(category._id)}
+                </div>
+              ))}
+            </div>
+            <div className="group relative">
+              <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 ">{post.title}</h3>
+              <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                {post.description}
+              </p>
+            </div>
+            <div className="relative mt-8 flex items-center gap-x-4">
+              {/* <img src={'jkbln'} alt="" className="h-10 w-10 rounded-full bg-gray-50" /> */}
+              <div className="text-sm leading-6">
+                {/* <p className="font-semibold text-gray-900">
                       <a href={post.author.href}>
                         <span className="absolute inset-0" />
                         {post.author.name}
                       </a>
                     </p> */}
-              {/* <p className="text-gray-600">{post.author.role}</p> */}
+                <Link href={post.externalLink}>
+                  <button className="rounded-md bg-primary px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">
+                    Gå til artikkelen
+                  </button>
+                </Link>
+              </div>
             </div>
-          </div>
-        </article>
+          </article>
+        </>
       ))}
     </div>
   )
