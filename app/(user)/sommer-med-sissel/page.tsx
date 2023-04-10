ffa0b66d47744be47b2e3f6f95fb525fd1d7604f
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-10 10:13:42 +0200

    styled summer post list

--- app/(user)/sommer-med-sissel/page.tsx
+++ app/(user)/sommer-med-sissel/page.tsx
@@ -1,6 +1,90 @@
+import SommerPost from 'components/sommerMedSissel/post'
+
+import { SummerPost } from 'type'
+
+const posts = [
+  {
+    _id: 'gkjhlkj',
+    title: 'Boost your conversion rate',
+    slug: '#',
+    description:
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+    imageUrl:
+      'https://premium.vgc.no/v2/images/5cd96f31-9262-4ae7-abd6-ba30e9bfa769?fit=crop&format=auto&h=1383&w=1900&s=43ceb61880aa7936e3207216f53c8949df82a1bd',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+    author: {
+      name: 'Michael Foster',
+      role: 'Co-Founder / CTO',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+  {
+    _id: 'ihjln',
+    title: 'Boost your conversion rate',
+    href: '#',
+    description:
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+    imageUrl:
+      'https://premium.vgc.no/v2/images/5cd96f31-9262-4ae7-abd6-ba30e9bfa769?fit=crop&format=auto&h=1383&w=1900&s=43ceb61880aa7936e3207216f53c8949df82a1bd',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+    author: {
+      name: 'Michael Foster',
+      role: 'Co-Founder / CTO',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+  {
+    _id: 'cyvub',
+    title: 'Boost your conversion rate',
+    href: '#',
+    description:
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+    imageUrl:
+      'https://premium.vgc.no/v2/images/5cd96f31-9262-4ae7-abd6-ba30e9bfa769?fit=crop&format=auto&h=1383&w=1900&s=43ceb61880aa7936e3207216f53c8949df82a1bd',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+    author: {
+      name: 'Michael Foster',
+      role: 'Co-Founder / CTO',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+  {
+    _id: 'hjklm',
+    title: 'Boost your conversion rate',
+    href: '#',
+    description:
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+    imageUrl:
+      'https://premium.vgc.no/v2/images/5cd96f31-9262-4ae7-abd6-ba30e9bfa769?fit=crop&format=auto&h=1383&w=1900&s=43ceb61880aa7936e3207216f53c8949df82a1bd',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+    author: {
+      name: 'Michael Foster',
+      role: 'Co-Founder / CTO',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+]
+
 export default function SummerWSissel() {
+  // TODO fix responsive rendering of images
   return (
-    <div className="overflow-hidden bg-white py-16 lg:py-32">
+    <div className=" bg-white py-16 lg:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-10 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
           <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
@@ -19,7 +103,7 @@ export default function SummerWSissel() {
             </p>
           </div>
           <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
-            <div className="hidden md:block w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
+            <div className="hidden lg:block w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
               <img
                 src="https://www.fjordnorway.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdjew0njor%2Fimage%2Fupload%2Far_1.5%2Cc_fill%2Cq_95%2Cw_1280%2Fv1%2FVisit%2520Region%2520Stavanger%2FGo%2520Fjords%2FPreikestolen-plateau-Go-Fjords-Bob-Engelsen-P1026771_kljg5o%3F_a%3DATAABAA0&w=1920&q=75"
                 alt=""
@@ -42,13 +126,24 @@ export default function SummerWSissel() {
                   className="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
                 />
               </div>
-              <div className="order-first flex w-64 flex-none lg:justify-end justify-center self-end md:self-center lg:w-auto">
+              <div className="order-first flex w-64 flex-none lg:justify-end justify-center self-end lg:self-center lg:w-auto">
                 <img
                   src="https://premium.vgc.no/v2/images/5cd96f31-9262-4ae7-abd6-ba30e9bfa769?fit=crop&format=auto&h=1383&w=1900&s=43ceb61880aa7936e3207216f53c8949df82a1bd"
                   alt=""
                   className="aspect-[4/3] lg:w-[24rem] w-[16rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
                 />
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:max-w-4xl">
+            <div className="space-y-16 lg:space-y-16">
+              {posts.map((post) => (
+                <div key={post._id}>{SommerPost(post)}</div>
+              ))}
             </div>
           </div>
         </div>
