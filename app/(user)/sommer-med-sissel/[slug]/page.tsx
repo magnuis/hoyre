066b6501891e2382c2749d9603113132aeea8ed7
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-10 13:19:26 +0200

    add back arrow

--- app/(user)/sommer-med-sissel/[slug]/page.tsx
+++ app/(user)/sommer-med-sissel/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { SummerPost } from 'type'
 import { groq } from 'next-sanity'
 import { PortableText } from '@portabletext/react'
 import { RichTextComponents } from 'components/shared/RichTextComponents'
+import BackArrow from 'components/shared/BackArrow'
 
 const builder = imageUrlBuilder(client)
 
@@ -41,15 +42,16 @@ export default async function SummerWSisselPost({ params: { slug } }: SummerWSis
       </div>
     )
   }
+
   return (
-    <div className="max-w-3xl mx-auto mt-16">
-      <div className="flex flex-col mx-6 gap-y-8 ">
+    <div className="max-w-7xl mx-auto sm:mt-16">
+      <div className="sm:absolute relative ml-6 ">
+        <BackArrow slug="/sommer-med-sissel" />
+      </div>
+      <div className=" max-w-3xl mx-auto flex flex-col gap-y-8 p-6">
         <p className="mx-auto text-4xl sm:text-5xl font-bold ">{summerPost.title}</p>
-        <time
-          dateTime={summerPost.date}
-          className="flex items-center text-base text-zinc-400 dark:text-zinc-500"
-        >
-          <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+        <time className="flex items-center text-base text-zinc-400 ">
+          <span className="h-4 w-0.5 rounded-full bg-zinc-200 " />
           <span className="ml-3">{summerPost.date}</span>
         </time>
         <div className="flex flex-col gap-y-6">
