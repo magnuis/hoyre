8f2c3f325665ed8d93a940d515019c5d8dc4fc7a
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-11 21:18:33 +0200

    add image carousel to landing page

--- components/shared/Carousel.tsx
+++ components/shared/Carousel.tsx
@@ -1,40 +1,54 @@
 'use client'
+import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
-const content = [
-  {
-    title: 'Title 1',
-    description: 'Description 1',
-    button: 'Button 1',
-    image:
-      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-  },
-  {
-    title: 'Title 2',
-    description: 'Description 2',
-    button: 'Button 2',
-    image:
-      'https://images.unsplash.com/photo-1544427920-c49ccfb85579?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1422&q=80',
-  },
-  {
-    title: 'Title 3',
-    description: 'Description 4',
-    button: 'Button 4',
-    image:
-      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-  },
-  {
-    title: 'Title 4',
-    description: 'Description 4',
-    button: 'Button 4',
-    image:
-      'https://images.unsplash.com/photo-1544427920-c49ccfb85579?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1422&q=80',
-  },
-]
+// const content = [
+//   {
+//     title: 'Title 1',
+//     description: 'Description 1',
+//     button: 'Button 1',
+//     image:
+//       'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+//   },
+//   {
+//     title: 'Title 2',
+//     description: 'Description 2',
+//     button: 'Button 2',
+//     image:
+//       'https://images.unsplash.com/photo-1544427920-c49ccfb85579?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1422&q=80',
+//   },
+//   {
+//     title: 'Title 3',
+//     description: 'Description 4',
+//     button: 'Button 4',
+//     image:
+//       'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+//   },
+//   {
+//     title: 'Title 4',
+//     description: 'Description 4',
+//     button: 'Button 4',
+//     image:
+//       'https://images.unsplash.com/photo-1544427920-c49ccfb85579?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1422&q=80',
+//   },
+// ]
 
-export default function Carousel(): JSX.Element {
+interface CarouselProps {
+  content: c[]
+}
+
+interface c {
+  content: any
+  title: string
+  description: string
+  button: string
+  image: string
+}
+
+export default function Carousel({ content }: CarouselProps): JSX.Element {
   const [cont, setCont] = useState(0)
   const [activeSlide, setActiveSlide] = useState(0)
+  console.log('Logger her', content[0].content.image)
 
   useEffect(() => {
     const xy = setInterval(() => {
@@ -51,7 +65,7 @@ export default function Carousel(): JSX.Element {
   }, [cont])
 
   return (
-    <div className="sliderAx h-auto relative ">
+    <div className="sliderAx relative " style={{ height: '700px' }}>
       {content.map((item, index) => (
         <div
           key={index + 1}
@@ -59,8 +73,9 @@ export default function Carousel(): JSX.Element {
           className={`absolute container top-0 left-0 w-full h-full duration-1000 ease-in-out transition-opacity ${
             activeSlide === index ? 'opacity-100' : 'opacity-0'
           }`}
+          style={{ height: '700px', width: '100%', maxWidth: '100%' }}
         >
-          <div
+          {/* <div
             className="bg-cover bg-center h-auto text-white py-24 px-10  "
             style={{
               backgroundImage: `url(${item.image})`,
@@ -81,7 +96,19 @@ export default function Carousel(): JSX.Element {
                 Button {`${index + 1}`}
               </button>
             </div>
-          </div>
+          </div>  <Image
+            priority
+            className="absolute object-center object-cover top-0 h-auto w-auto opacity-100 mx-auto"
+            src={item.content.image}
+            alt={'Landing page image'}
+            fill
+          /> */}
+
+          <img
+            src={item.content.image}
+            alt=""
+            className="sm:aspect-[9/7] md:aspect-[9/5] aspect-[1] sm:w-max bg-gray-50 object-cover"
+          />
         </div>
       ))}
     </div>
