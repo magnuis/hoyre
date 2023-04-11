2e9f9c6c67a73832f001791377a4a5ff5b51c255
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-11 12:48:02 +0200

    add sanity support for blog posts

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,8 +1,9 @@
 import blockContent from './blockContent'
-import category from './category'
+import category from './subject'
 import post from './post'
 import author from './author'
 import navigation from './navigation'
 import summerPost from './summerPost'
+import blogBost from './blogBost'
 
-export const schemaTypes = [post, author, category, summerPost, navigation, blockContent]
+export const schemaTypes = [post, author, category, summerPost, navigation, blockContent, blogBost]
