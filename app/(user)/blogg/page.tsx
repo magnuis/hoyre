93920ddbfbb43f99ad6f3a62dc281a178e1e55d0
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-11 15:53:48 +0200

    style blog home pgae

--- app/(user)/blogg/page.tsx
+++ app/(user)/blogg/page.tsx
@@ -1,37 +1,19 @@
-import SummerPostCard, { summerPostCardProps } from 'components/sommerMedSissel/SummerPostCard'
-import { groq } from 'next-sanity'
-import { client } from 'sanity-conf/sanity.client'
-import imageUrlBuilder from '@sanity/image-url'
 import BlogPostsList from './blogPostCardList'
 
-const builder = imageUrlBuilder(client)
-
 export default async function Blogg() {
-  const postQuery = groq`
-*[_type=='blogPost'] {
-title, slug, image, description, date, _id 
-} `
-  const imgQuery = groq`*[_type == "sanity.imageAsset" && references("SmPITB92PCr5r7guzAyPD2")] {
-  url,
-  alt,
-  _id
-}`
-
-  const summerPosts = await client.fetch(postQuery)
-  const images = await client.fetch(imgQuery)
-
   // TODO fix responsive rendering of images
   return (
     <div className=" bg-white py-16 lg:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
+      <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8 ">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-10 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
-          <div className="mx-auto max-w-2xl">
-            <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
-              Artikler om Stavanger Høyre
-            </h2>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
-              Her har vi samlet et par artikler der Stavanger Høyre og Sissel er omtalt, slik at du
-              kan få med deg hva som blir sagt.
+          <div className="mx-auto max-w-2xl ">
+            <div className="text-center">
+              <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl mx-auto">Bloggen</h2>
+            </div>
+
+            <p className="mt-8 text-lg leading-8 text-gray-600">
+              Har du lurt på hva Sissel tenker om Stavangerskolen? Eller hvordan hun tenker å takle
+              eldrebølgen? Her kan du lese om det og mye mer.
             </p>
           </div>
           <BlogPostsList />
