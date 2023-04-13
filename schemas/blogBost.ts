3198fad7edfa5565abab8293ec927daf476c43e1
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-13 16:40:20 +0200

    rename page

--- schemas/blogBost.ts
+++ schemas/blogBost.ts
@@ -2,7 +2,7 @@ import { defineField, defineType } from 'sanity'
 
 export default defineType({
   name: 'blogPost',
-  title: 'Blogg',
+  title: 'Smaken av Stavanger',
   type: 'document',
   fields: [
     defineField({
