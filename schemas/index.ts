a62bdbc0f3456a5ad7f6130f2f6f8f54492c2580
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-14 12:50:39 +0200

    fetch timeline objects from sanity

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,5 +4,14 @@ import post from './post'
 import author from './author'
 import navigation from './navigation'
 import summerPost from './summerPost'
+import sisselTimeline from './sisselTimeline'
 
-export const schemaTypes = [post, author, category, summerPost, navigation, blockContent]
+export const schemaTypes = [
+  post,
+  author,
+  category,
+  summerPost,
+  navigation,
+  blockContent,
+  sisselTimeline,
+]
