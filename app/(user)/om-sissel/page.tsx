290364f716ff476ab21676a816cbb8812048e34f
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-16 10:18:18 +0200

    temp fix

--- app/(user)/om-sissel/page.tsx
+++ app/(user)/om-sissel/page.tsx
@@ -6,42 +6,37 @@ import Image from 'next/image'
 import { client } from 'sanity-conf/sanity.client'
 import { SisselTimeline } from 'type'
 import StoryContent, { StoryContentProps } from './StoryContent'
+import ImageGallery from 'react-image-gallery'
 
-// let storyContent = [
-//   {
-//     title: 'BARNDOM I HARSTAD:',
-//     text: 'Vi har oppnådd mye. Jeg er stolt av det Stavanger vi sammen har skapt, fra å være oljehovedstad til energihovedstad. Jeg har et brennende engasjement for næringslivet. Nye arbeidsplasser skal vokse frem på skuldrene av olje- og gasseventyret. Nå blir det en viktig jobb å skaffe arbeidskraften vi trenger for å ta steget videre inn i det grønne skiftet.',
-//     img: 'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-//   },
-//   {
-//     title: 'UNGDOMSÅRENE:',
-//     text: 'Vi har oppnådd mye. Jeg er stolt av det Stavanger vi sammen har skapt, fra å være oljehovedstad til energihovedstad. Jeg har et brennende engasjement for næringslivet. Nye arbeidsplasser skal vokse frem på skuldrene av olje- og gasseventyret. Nå blir det en viktig jobb å skaffe arbeidskraften vi trenger for å ta steget videre inn i det grønne skiftet.',
-//     img: 'https://hoyre.no/content/uploads/sites/212/2022/09/20220907_123329386_iOS-scaled.jpg',
-//   },
-//   {
-//     title: 'VEIEN INN I POLITIKKEN:',
-//     text: 'Vi har oppnådd mye. Jeg er stolt av det Stavanger vi sammen har skapt, fra å være oljehovedstad til energihovedstad. Jeg har et brennende engasjement for næringslivet. Nye arbeidsplasser skal vokse frem på skuldrene av olje- og gasseventyret. Nå blir det en viktig jobb å skaffe arbeidskraften vi trenger for å ta steget videre inn i det grønne skiftet.',
-//     img: '',
-//   },
-//   {
-//     title: 'FAMILIELIV:',
-//     text: 'Vi har oppnådd mye. Jeg er stolt av det Stavanger vi sammen har skapt, fra å være oljehovedstad til energihovedstad. Jeg har et brennende engasjement for næringslivet. Nye arbeidsplasser skal vokse frem på skuldrene av olje- og gasseventyret. Nå blir det en viktig jobb å skaffe arbeidskraften vi trenger for å ta steget videre inn i det grønne skiftet.',
-//     img: 'https://hoyre.no/content/uploads/sites/212/2022/09/20220907_123329386_iOS-scaled.jpg',
-//   },
-// ]
 export default async function AboutSissel() {
   const storyQuery = groq`
-    *[_type == "sisselTimeline"] {
+   *[_type == "sisselTimeline"] {
         title,
         content,
+        date,
         "img": image.asset->url
-    } | order(date desc)
+    } | order(date asc)
     `
 
+  const imgQuery = groq`
+*[_type == "sanity.imageAsset" && references('f5T2DmY3fkMt6I6qKWVOvS')] {
+url
+} 
+`
+  interface a {
+    original: string
+    thumbnail: string
+  }
   const storyContent: SisselTimeline[] = await client.fetch(storyQuery)
-
+  const images = await client.fetch(imgQuery)
   const lastStory = storyContent.pop()
-  console.log(lastStory)
+  const galleryImages: a[] = images.map((image: any) => {
+    return {
+      original: image.url,
+      thumbnail: image.url,
+    }
+  })
+
   return (
     <div className="max-w-7xl mx-auto sm:mt-16">
       {/* <div className="sm:absolute relative ml-6 ">
@@ -56,20 +51,20 @@ export default async function AboutSissel() {
       >
         <p className="text-white text-4xl ">Bli kjent med Sissel</p>
       </div>
+      <div className="text-center max-w-lg mx-auto my-24">
+        <p className="font-bold text-2xl">
+          Sissel er Høyres ordførerkandidat i Stavanger. Hun er mor til tre - Espen, Emilie og
+          Fredrik.
+        </p>
+      </div>
+      <Image
+        src="https://premium.vgc.no/v2/images/ea71d845-a98a-47af-a34f-7623b5e1b7b1?fit=crop&format=auto&h=1425&w=1900&s=e10bed9c5cbfa2e48d588258d2cf5f208e0442aa"
+        alt=""
+        width={900}
+        height={900}
+        className="container h-[50vh] object-cover object-center"
+      />
       <div className="mx-auto flex flex-col gap-y-8 p-6">
-        <div className="text-center max-w-lg mx-auto my-24">
-          <p className="font-bold text-2xl">
-            Sissel er Høyres ordførerkandidat i Stavanger. Hun er mor til tre - Espen, Emilie og
-            Fredrik.
-          </p>
-        </div>
-        <Image
-          src="https://premium.vgc.no/v2/images/ea71d845-a98a-47af-a34f-7623b5e1b7b1?fit=crop&format=auto&h=1425&w=1900&s=e10bed9c5cbfa2e48d588258d2cf5f208e0442aa"
-          alt=""
-          width={900}
-          height={900}
-          className="container h-[50vh] object-cover object-center"
-        />
         <div className="relative max-w-xl mx-auto">
           {storyContent.map((story, index) => (
             <div key={index}>
@@ -82,6 +77,19 @@ export default async function AboutSissel() {
               <PortableText value={lastStory?.content} components={RichTextComponents} />
             </div>
             <div className="absolute top-0 transform h-3 w-3 bg-white rounded-full border border-primary translate-x-[-6.5px]" />
+            <div className="ml-8">
+              {/* <img
+                src={images[0].url}
+                alt=""
+                className="rounded-t-lg object-cover object-center h-[30vh] w-full"
+              />
+              <div className="flex flex-row justify-between mt-[3%]">
+                <img src={images[1].url} alt={''} className="relative w-[31%]" />
+                <img src={images[2].url} alt={''} className="relative w-[31%]" />
+                <img src={images[3].url} alt={''} className="relative w-[31%]" />
+              </div> */}
+              {/* <ImageGallery items={galleryImages} /> */}
+            </div>
           </div>
           <div className="rounded-xl bg-primary">
             <img
@@ -89,7 +97,7 @@ export default async function AboutSissel() {
               alt=""
               className="rounded-t-lg object-cover object-top h-[30vh] w-full"
             />
-            <div className="flex flex-col gap-y-5 text-white p-5">
+            <div className="relative flex flex-col gap-y-5 text-white p-5">
               <div className="text-center text-2xl font-bold">
                 <p>Følg Sissel i hverdagen</p>
               </div>
