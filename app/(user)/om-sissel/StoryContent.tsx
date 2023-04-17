63b32f06635d7395de06585bfe58ee76253e1fc7
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-17 23:50:48 +0200

    minor style fixes

--- app/(user)/om-sissel/StoryContent.tsx
+++ app/(user)/om-sissel/StoryContent.tsx
@@ -6,16 +6,23 @@ export interface StoryContentProps {
   content: any
   title: string
   img: string
+  last: boolean
 }
 export default function StoryContent(props: StoryContentProps): JSX.Element {
   return (
-    <div className="group border-l border-primary pb-8 relative">
-      <div className="pl-8 flex flex-col gap-y-5">
+    <div
+      className={`group ${props.last ? '' : 'border-l'} border-primary pb-4 ml-2 md:m-0 relative`}
+    >
+      <div className="pl-4 md:pl-10 flex flex-col gap-y-4">
         <p className="text-gray-500 text-xs">{props.title.toUpperCase()}</p>
         <PortableText value={props.content} components={RichTextComponents} />
         {props.img && <Image src={props.img} alt={''} height={500} width={500}></Image>}
       </div>
-      <div className="absolute top-0 transform md:h-3 h-4 md:w-3 w-4 bg-white group-hover:bg-primary rounded-full border border-primary translate-x-[-8px] md:translate-x-[-6.5px]" />
+      <div
+        className={`absolute top-0 transform md:h-3.5 h-4 md:w-3.5 w-4 bg-white group-hover:bg-primary rounded-full border border-primary translate-x-[-8px] ${
+          props.last ? 'md:translate-x-[-6.5px]' : ''
+        }`}
+      />
     </div>
   )
 }
