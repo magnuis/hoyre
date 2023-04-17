2e85545ee459096b131a32913285d557bcd5acad
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-17 22:01:51 +0200

    remove unused imports

--- app/(user)/om-sissel/FollowSisselNavCard.tsx
+++ app/(user)/om-sissel/FollowSisselNavCard.tsx
@@ -9,13 +9,12 @@ export default function FollowSisselNavCard() {
         className="rounded-t-lg object-cover object-top h-[30vh] w-full"
       />
       <div className="relative flex flex-col gap-y-7 text-white p-5 text-center items-center">
-        <p className="text-2xl font-bold">Følg Sissel i hverdagen</p>
+        <p className="text-2xl font-bold">Følg Sissel i hverdagen:</p>
         <div className="flex flex-wrap gap-3 justify-center">
           <Button text="FACEBOOK" href="https://www.facebook.com/sissel.k.hegdal" />
           <Button text="INSTAGRAM" href="https://www.instagram.com/sisselhegdal/" />
           <Button text="LINKEDIN" href="https://www.linkedin.com/in/sissel-knutsen-hegdal/" />
         </div>
-
         <Button
           text="SISSELS PERSONPROFIL (HØYRE.NO)"
           href="https://hoyre.no/stavanger/sissel-knutsen-hegdal-var-ordforerkandidat/"
--- app/(user)/om-sissel/StoryContent.tsx
+++ app/(user)/om-sissel/StoryContent.tsx
@@ -9,14 +9,13 @@ export interface StoryContentProps {
 }
 export default function StoryContent(props: StoryContentProps): JSX.Element {
   return (
-    <div className="border-l border-primary pb-8 relative">
+    <div className="group border-l border-primary pb-8 relative">
       <div className="pl-8 flex flex-col gap-y-5">
-        <p className="text-gray-500 text-xs">{props.title}</p>
-        {/* <p>{props.content}</p> */}
+        <p className="text-gray-500 text-xs">{props.title.toUpperCase()}</p>
         <PortableText value={props.content} components={RichTextComponents} />
         {props.img && <Image src={props.img} alt={''} height={500} width={500}></Image>}
       </div>
-      <div className="absolute top-0 transform h-3 w-3 bg-white rounded-full border border-primary translate-x-[-6.5px]" />
+      <div className="absolute top-0 transform md:h-3 h-4 md:w-3 w-4 bg-white group-hover:bg-primary rounded-full border border-primary translate-x-[-8px] md:translate-x-[-6.5px]" />
     </div>
   )
 }
