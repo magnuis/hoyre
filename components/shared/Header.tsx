69330df047b2ebcfa6e51fc942077fca94af23a2
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-17 17:10:16 +0200

    rename page and update header routing

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -5,11 +5,9 @@ import Link from 'next/link'
 import MobileMenu from './MobileMenu'
 
 const navigation = [
-  { name: 'Partiprogram', href: '/partiprogram' },
   { name: 'Smaken av Stavanger', href: '/smaken-av-stavanger' },
   { name: 'Sommer med Sissel', href: '/sommer-med-sissel' },
-  { name: 'Blogg', href: '/blogg' },
-  { name: 'Artikler', href: '/eksterne-artikler' },
+  { name: 'Høyre i media', href: '/hoyre-i-media' },
 ]
 
 export default function Header() {
