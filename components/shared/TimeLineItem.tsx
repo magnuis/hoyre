02aa2e80ecc6c038d3f36b30416b5d8f4b9ee9f0
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-04-20 23:09:46 +0200

    refactor TimeLineComponent

--- components/shared/TimeLineItem.tsx
+++ components/shared/TimeLineItem.tsx
@@ -13,15 +13,21 @@ export interface timelineItemProps {
 }
 export default function TimelineItem(props: timelineItemProps): JSX.Element {
   return (
-    <div className={`${props.last ? '' : 'border-l border-primary'} pb-8 relative`}>
-      <div className="pl-8 flex flex-col gap-y-5">
+    <div
+      className={`group ${props.last ? '' : 'border-l'} border-primary pb-4 ml-2 md:m-0 relative`}
+    >
+      <div className="pl-4 md:pl-10 flex flex-col gap-y-4">
         <p className="text-gray-500 text-xs">{props.title.toUpperCase()}</p>
         <PortableText value={props.content} components={RichTextComponents} />
         {props.img && (
           <img src={builder.image(props.img).url()} alt={''} height={500} width={500} />
         )}
       </div>
-      <div className="absolute top-0 transform h-3 w-3 bg-white rounded-full border border-primary translate-x-[-6.5px]" />
+      <div
+        className={`absolute top-0 transform md:h-3.5 h-4 md:w-3.5 w-4 bg-white group-hover:bg-primary rounded-full border border-primary translate-x-[-8px] ${
+          props.last ? 'md:translate-x-[-6.5px]' : ''
+        }`}
+      />
     </div>
   )
 }
